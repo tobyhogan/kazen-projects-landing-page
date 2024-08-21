80f85b9d2a742f0849c476fb9bf22defe39e3211
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-08-21 13:55:38 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,8 @@ const IndexPage: React.FC<PageProps> = () => {
         <h2 id="clientwork">Areas:</h2>
         <ul>
           <h3>Kazen Software - Enterprise Projects, see more <a href="https://tobyhogan.space" className="Link3" target="_blank">here</a></h3>
-          <h3><a href="https://tobyhogan.github.io/consulting-landing-page" className="Link3" target="_blank">Kazen Consulting</a> - Business & Small Business</h3>
+          <h3><a href="https://tobyhogan.github.io/consulting-landing-page" className="Link3" target="_blank">Kazen Consulting</a> - Solutions for Business and Individuals</h3>
+          <h3><a href="https://tobyhogan.github.io/consulting-landing-page" className="Link3" target="_blank">Kazen Open</a> - Open Source Systems and Technology</h3>
         </ul>
         <h2 id="clientwork">Client Work:</h2>
         <h3>Full:</h3>
